93e6ea0a58ce2d99ffb002a13b72289082084461
Lea Rosema <lea@lea.codes>
2023-11-24 01:05:38 +0100

    feat: add a little microsite

--- plugins/index.js
+++ plugins/index.js
@@ -1,9 +1,10 @@
 const esbuild = require('./esbuild');
 const htmlTransform = require('./html-transform');
 const lightningCss = require('./lightning-css');
-
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 module.exports = (eleventyConfig) => {
   eleventyConfig.addPlugin(esbuild);
   eleventyConfig.addPlugin(htmlTransform);
   eleventyConfig.addPlugin(lightningCss);
+	eleventyConfig.addPlugin(syntaxHighlight);
 };
